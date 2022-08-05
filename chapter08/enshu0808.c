1119c35c4a6b586928ea9bd2f1bce66f7f58157a
keeykeey <sh1n_fuj1to@icloud.com>
2022-08-06 00:14:12 +0900

    ユークリットの互除法

---
+++ chapter08/enshu0808.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int max(int dx,int dy) {
+    return ((dx >= dy) ? dx : dy);
+}
+
+int min(int dx,int dy) {
+    return ((dx>=dy) ? dy : dx);
+}
+
+int get_diff(int longer,int shorter) {
+    int diff = longer;
+    while (diff > shorter) {
+        diff = diff - shorter; 
+    }
+    return diff;
+}
+
+int gcd(int x, int y) {
+    int longer = max(x,y);
+    int shorter = min(x,y);
+
+    
+    /*shorter = get_diff(longer,shorter);
+
+    do{
+        int tmp_longer = longer;
+        longer = shorter;
+        shorter = get_diff(tmp_longer,shorter);
+    }while(longer > shorter);*/
+
+    while(longer != shorter){
+        int tmp = longer;
+        longer = shorter;
+        shorter = get_diff(tmp,shorter);
+    }
+
+    return shorter;//最大公約数    
+}
+
+int main(void) {
+    int dx,dy;
+    printf("一つ目の長さを入力 : "); scanf("%d",&dx);
+    printf("二つ目の長さを入力 : "); scanf("%d",&dy);
+    int ret = gcd(dx,dy);
+    printf("main function :%d\n",ret);
+}
+
+
