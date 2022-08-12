dfc1e720a784a0136edf70980f0252cb5d9c2f37
keeykeey <sh1n_fuj1to@icloud.com>
2022-08-12 22:35:00 +0900

    受け取った文字列の配列に格納されているn個の文字列の文字の並びを反転する

--- chapter09/enshu0912.c
+++ chapter09/enshu0912.c
@@ -9,15 +9,19 @@ int get_str_length(char s[]) {
     return i;
 }
 
+void swap_strs(char s[],int n){
+    char tmp = s[n];
+    s[n] = s[n+1];
+    s[n+1] = tmp;
+}
+
 void rev_string(char s[][STR_LENGTH],int n){
     for (int i=0; i<n; i++) {
         int len = get_str_length(s[i]);
         while(len-1){
             int j=0;
             for (j=0;j<len-2;j++){
-                char tmp = s[i][j];
-                s[i][j] = s[i][j+1];
-                s[i][j+1] = tmp;
+                swap_strs(s[i],j);
             }
             len--;
         }
